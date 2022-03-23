d539339f8c12b91bd75216e04f97cbbc875d5cc3
Daniel <danielw96@hotmail.com>
2022-03-23 15:41:00 -0400

    Question Fragment started
    need to make models for this

--- app/src/main/java/com/example/quiz_application_multi_lingual/MainActivity.java
+++ app/src/main/java/com/example/quiz_application_multi_lingual/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.quiz_application_multi_lingual;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager fm = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //below is an example, make work for this assignment
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.QuestionFragmentContainer2, QuestionFragment.class, null)
+                .commit();
     }
 }
