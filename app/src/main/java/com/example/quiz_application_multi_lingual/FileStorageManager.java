91be5804eccdfb87032fbe977e9b8d62d2d0dc38
Daniel <danielw96@hotmail.com>
2022-03-29 22:52:42 -0400

    file writing functionality

--- app/src/main/java/com/example/quiz_application_multi_lingual/FileStorageManager.java
+++ app/src/main/java/com/example/quiz_application_multi_lingual/FileStorageManager.java
@@ -2,14 +2,20 @@ package com.example.quiz_application_multi_lingual;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.Log;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class FileStorageManager {
     static String fileName = "Average.txt";
     FileOutputStream fos;
+    FileInputStream fis;
 
     public void updateAverage(Activity context, String newAverage){
         try{
@@ -30,8 +36,35 @@ public class FileStorageManager {
 
 
     }
-    public String getAverage(){
-        //temp 
+    //todo fix this
+    public String getAverage(Activity context){
+        String contents;
+        String line;
+        try {
+            fis = context.openFileInput(fileName);
+            InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);//what dis
+            StringBuilder stringBuilder = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(inputStreamReader)){
+               /*
+                line = reader.readLine();
+                while (line!= null) {
+                    stringBuilder.append(line).append()
+                }
+                */
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+
+                //contents = StringBuilder.toString();
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        //temp
         String average = "0/0";
         return average;
     }
